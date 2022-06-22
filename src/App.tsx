4e7e2c96c82533e66aea544b68550021cd08e49f
Mike Wells <micheal.w.wells@gmail.com>
2022-06-22 13:12:23 -0700

    demo updates

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,13 @@ import { useState, useEffect } from "react";
 import { DataBCShapes } from "./DataBCShapes";
 import { FeatureCollection } from "@turf/turf";
 import { TurfGeos } from "./TurfGeos";
-import {CRS} from 'leaflet';
 
 
 export default function App() {
-  const [map, setMap] = useState<any>(null);
   const [DataBCGeos, setDataBCGeos] = useState(null)
 
 
   // search boundaries are blue - data filtered by databc is green, and red is filtered by turf.
-
   // featureCollection.features[0] = shape we send to databc
   // featureCollection.features[1] = smaller shape we test for intersect with turf
   const featureCollection: FeatureCollection = {
@@ -92,9 +89,7 @@ export default function App() {
       center={[55, -122]}
       zoom={5}
       scrollWheelZoom={true}
-      editable={true}
       zoomControl={true}
-      whenCreated={setMap}
     >
       <ZoomControl position="bottomleft" />
       <TileLayer
@@ -102,11 +97,9 @@ export default function App() {
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
 
-      {/*map && <DrawControl map={map} setGeometry={setGeometry} />*/}
-      {featureCollection && <GeoJSON data={featureCollection} key={Math.random()} />}
-      
-      {featureCollection && <DataBCShapes layerName={'WHSE_WATER_MANAGEMENT.WLS_WATER_RESERVES_POLY'} geometry={featureCollection.features[0]} shapeSetterCallback={setDataBCGeos}/>}
-      {featureCollection && DataBCGeos &&  <TurfGeos data={DataBCGeos} filterGeo={featureCollection.features[1]}/>}
+      {/*featureCollection && <GeoJSON data={featureCollection} key={Math.random()} />*/}
+      {/*featureCollection && <DataBCShapes layerName={'WHSE_WATER_MANAGEMENT.WLS_WATER_RESERVES_POLY'} geometry={featureCollection.features[0]} shapeSetterCallback={setDataBCGeos}/>*/}
+      {/*featureCollection && DataBCGeos &&  <TurfGeos data={DataBCGeos} filterGeo={featureCollection.features[1]}/>*/}
     </MapContainer>
   );
 }
